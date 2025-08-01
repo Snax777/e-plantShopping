12f48c03a8a44a4ea859d851104a41592d86cc1c
Snax777 <sinethembatompelo3@gmail.com>
2025-08-01 13:33:32 +0200

    Task 2 completed

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,30 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+      let newItem = {
+        name: action.payload.name,
+        image: action.payload.image,
+        cost: action.payload.cost,
+        quantity: 1,
+      };
+
+      let existingItem = state.items.find(item => item.name === newItem.name);
+
+      if (existingItem) {
+        existingItem.quantity++
+      } else {
+        state.items.push(newItem);
+      }
     },
     removeItem: (state, action) => {
+      state.items = state.items.filter(item => item.name !== action.payload.name);
     },
     updateQuantity: (state, action) => {
+      let existingItem = state.items.find(item => action.payload.name === item.name);
 
-    
+      if (existingItem) {
+        existingItem.quantity = action.payload.quantity;
+      }
     },
   },
 });
